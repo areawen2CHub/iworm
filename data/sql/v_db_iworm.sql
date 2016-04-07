0a0627ce3fc15a28e3cb4bdb141cfe55193df0a2
unknown <>
2016-04-07 17:39:24 +0800

    整改数据库结构，初步完成

--- data/sql/v_db_iworm.sql
+++ data/sql/v_db_iworm.sql
@@ -23,60 +23,69 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- 表的结构 `v_db_infourl`
+-- 表的结构 `v_db_host`
 --
-
-CREATE TABLE IF NOT EXISTS `v_db_infourl` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'urlid',
-  `parenturl` varchar(255) NOT NULL COMMENT '来源地址',
-  `url` varchar(255) NOT NULL COMMENT '链接地址',
-  `urlip` varchar(30) NOT NULL COMMENT '链接ip',
-  `urlhost` varchar(255) NOT NULL COMMENT '链接主机',
-  `createtime` int(10) NOT NULL COMMENT '收录时间',
-  `inctimes` mediumint(8) unsigned NOT NULL COMMENT '收录次数',
-  `incstate` enum('true','false') NOT NULL COMMENT '摘取状态',
-  `delstate` enum('true','false') NOT NULL COMMENT '删除状态',
-  `deltime` int(10) unsigned NOT NULL COMMENT '删除时间',
+CREATE TABLE IF NOT EXISTS `v_db_host` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '关键字表id',
+  `hostname` VARCHAR(50) NOT NULL COMMENT '主机名',
+  `hostip` VARCHAR(30) NOT NULL COMMENT '主机对应ip',
+  `urlcount` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT 'url收录数',
+  `inccount` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT '获取文章数',
+  `inittime` INT(10) UNSIGNED NOT NULL COMMENT '第一次收录时间',
+  `neartime` INT(10) UNSIGNED NOT NULL COMMENT '最近收录时间',
+  `errcount` MEDIUMINT(4) UNSIGNED NOT NULL COMMENT '出错次数',
+  `isusing` ENUM('true', 'false') NOT NULL COMMENT '主机状态，是否还在运行',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
+  ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 --
--- 表的结构 `v_db_infourl`
+-- 表的结构 `v_db_url`
 --
-
-
-CREATE TABLE IF NOT EXISTS `v_db_infoarticle` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '文章id',
-  `classid` smallint(5) unsigned NOT NULL COMMENT '文章所属分类id',
-  `title` varchar(80) NOT NULL COMMENT '标题',
-  `flag` varchar(30) NOT NULL COMMENT '属性',
-  `isoriginal` enum('true','false') NOT NULL COMMENT '是否原创',
-  `author` varchar(50) NOT NULL COMMENT '作者编辑',
-  `ourl` varchar(255) NOT NULL COMMENT '来源链接',
-  `keywords` varchar(50) NOT NULL COMMENT '关键词',
-  `description` varchar(255) NOT NULL COMMENT '摘要',
-  `content` mediumtext NOT NULL COMMENT '详细内容',
-  `picurl` varchar(100) NOT NULL COMMENT '缩略图片',
-  `picarr` text NOT NULL COMMENT '组图',
-  `hits` mediumint(8) unsigned NOT NULL COMMENT '点击次数',
-  `orderid` int(10) unsigned NOT NULL COMMENT '排列排序',
-  `createtime` int(10) NOT NULL COMMENT '更新时间',
-  `checkinfo` enum('true','false') NOT NULL COMMENT '审核状态',
-  `delstate` enum('true','false') NOT NULL COMMENT '删除状态',
-  `deltime` int(10) unsigned NOT NULL COMMENT '删除时间',
+CREATE TABLE IF NOT EXISTS `v_db_url` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'urlid',
+  `hostid` INT(10) UNSIGNED NOT NULL,
+  `parenturl` VARCHAR(255) NOT NULL COMMENT '来源地址',
+  `url` VARCHAR(255) NOT NULL COMMENT '链接地址',
+  `createtime` INT(10) UNSIGNED NOT NULL COMMENT '收录时间',
+  `inctimes` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT '收录次数',
+  `incstate` ENUM('true', 'false') NOT NULL COMMENT '收录状态',
+  `incsuccess` ENUM('true', 'false') NOT NULL COMMENT '是否收录成功',
+  `delstate` ENUM('true', 'false') NOT NULL COMMENT '删除状态',
+  `deltime` INT(10) NOT NULL COMMENT '删除时间',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
+  ) ENGINE=MyISAM AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8;
 
 --
--- 表的结构 `v_db_infcharset`
+-- 表的结构 `v_db_infolist`
 --
+CREATE TABLE IF NOT EXISTS `v_db_infolist` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'urlid',
+  `classid` SMALLINT(5) UNSIGNED NOT NULL COMMENT '文章分类id',
+  `title` VARCHAR(80) NOT NULL COMMENT '文章标题',
+  `flag` VARCHAR(30) NOT NULL COMMENT '属性',
+  `isoriginal` ENUM('false', 'true') NOT NULL COMMENT '是否原创',
+  `author` VARCHAR(50) NOT NULL COMMENT '收录时间',
+  `urlid` INT(10) UNSIGNED NOT NULL,
+  `keywords` VARCHAR(50) NOT NULL COMMENT '收录状态',
+  `description` VARCHAR(255) NOT NULL COMMENT '摘要',
+  `content` MEDIUMTEXT NOT NULL COMMENT '详细内容',
+  `picurl` VARCHAR(100) NOT NULL COMMENT '缩略图',
+  `picarr` TEXT NOT NULL COMMENT '图组',
+  `hits` MEDIUMINT(8) NOT NULL COMMENT '浏览数',
+  `orderid` INT(10) UNSIGNED NOT NULL COMMENT '排序',
+  `createtime` INT(10) UNSIGNED NOT NULL COMMENT '创建时间',
+  `checkinfo` ENUM('true', 'false') NOT NULL COMMENT '审核情况',
+  `delstate` ENUM('false', 'true') NOT NULL COMMENT '删除状态',
+  `deltime` INT(10) NOT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`)
+  ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
+--
+-- 表的结构 `v_db_charset`
+--
 CREATE TABLE IF NOT EXISTS `v_db_charset` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'charsetid',
-  `charset` varchar(50) NOT NULL COMMENT 'charset',
-  `host` varchar(255) NOT NULL COMMENT '来源主机',
+  `id` SMALLINT(10) UNSIGNED NOT NULL COMMENT '关键字表id',
+  `hostid` INT(10) UNSIGNED NOT NULL,
+  `charset` VARCHAR(10) NOT NULL COMMENT '关键字',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
+  ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
