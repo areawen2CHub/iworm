0224ed5905eb9bfc65b487d666b206f5cf2cb255
unknown <>
2016-04-13 12:50:47 +0800

    让程序在后台运行，不限制响应时间

--- iworm.php
+++ iworm.php
@@ -1,6 +1,6 @@
 <?php require_once(dirname(__FILE__).'/include/fore.entr.php'); // 引入前段入口文件
 ?>
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="zh_CN">
 <head>
 <meta charset="utf-8">
@@ -10,39 +10,42 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0 , maximum-scale=1.0, user-scalable=0">
 	<title><?php echo $title ?></title>
 <!-- <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet"> -->
-<link rel="stylesheet" href="css/subgroups.base.css" media="screen">
+<!-- <link rel="stylesheet" href="css/subgroups.base.css" media="screen">
 <link rel="stylesheet" href="css/subgroups.main.css">
 <link rel="stylesheet" href="css/font-awesome.min.css">
 </head>
-<body>
+<body> --> 
 
 <?php
-// 抓取网页
-$i = 0;
-$j = 0;
-$sql = "select u.id as id,u.hostid as hostid,u.url as url,h.inccount as inccount,h.errcount as errcount 
-from v_db_url u left join v_db_host h on u.hostid = h.id where 1=1 
-and u.incstate='false' 
-and u.delstate='false'
-and u.isempty = 'false'
-and h.isusing ='true'
-order by h.inccount desc,h.errcount asc LIMIT 0,10";
-$dosql->Execute($sql);
-while($row = $dosql->GetArray()){
-	if(!empty($row['id']) && !empty($row['hostid']) && !empty($row['url'])){
-		if($i>=10){
-			break;
-		}else{
-			$html = new HTML($row['id'],$row['hostid'],$row['url']);
-		    if($html->status){
-			    $i++;
-		    }
-		}
-	}
-	$j++;
+// 后台运行
+ignore_user_abort(true);
+// 响应时间无上线
+set_time_limit(0);
+while (true) {
+	// 抓取网页
+    $i = 0;
+    $j = 0;
+    $dosql->Execute("select * from vi_url limit 0,100");
+    while($row = $dosql->GetArray()){
+    	if(!empty($row['id']) && !empty($row['hostid']) && !empty($row['url'])){
+    		if($i>=10){
+    			break;
+    		}else{
+    			$html = new HTML($row['id'],$row['hostid'],$row['url']);
+    		    if($html->status){
+    			    $i++;
+    		    }
+    		}
+    	}
+    	$j++;
+    }
+    // 休眠1分钟
+    sleep(60);
 }
-echo $j.'<br />';
-echo $i;
+
+
+// echo $j.'<br />';
+// echo $i;
 ?>
-</body>
-</html>
+<!-- </body>
+</html> -->
