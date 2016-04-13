c61d978ee2a102ecc48f42287b980949c1da190e
unknown <>
2016-04-13 18:15:49 +0800

    提交数据库操作记录文件

---
+++ data/sql/sql操作记录.sql
@@ -0,0 +1,68 @@
+/*2016-04-13*/
+alter table v_db_url add isempty ENUM('false', 'true') NOT NULL COMMENT '内容是否为空';
+alter table v_db_url add lasttime int(10) UNSIGNED NOT NULL COMMENT '上一次访问时间';
+alter table v_db_url add neartime int(10) UNSIGNED NOT NULL COMMENT '最近访问时间';
+alter table v_db_url add viscount MEDIUMINT(8) UNSIGNED NOT NULL COMMENT '访问数';
+alter table v_db_keywords change searchtimes seacount MEDIUMINT(8);
+alter table v_db_url change inctimes inccount MEDIUMINT(8);
+
+update v_db_url set neartime=createtime+24*60 where incstate = 'true' and id>0;
+update v_db_url set viscount=1 where incstate = 'true' and id>0;
+
+/*查看profiles*/
+show profiles;
+/*显示查询的变量*/
+show variables like '%pro%';
+/*开启profile*/
+set profiling = 1;
+/*更改字段属性*/
+alter table v_db_charset modify hostid int(10);
+
+/*删除host表urlcount和inccount字段*/
+alter table v_db_host drop column urlcount,drop column inccount;
+/*删除host表neartime字段*/
+alter table v_db_host drop column neartime;
+/*更新host表errcount字段类型*/
+alter table v_db_host drop column errcount;
+/*更新url表incsuccess字段类型*/
+alter table v_db_url modify incsuccess enum('false','true');
+update v_db_url set incsuccess=null where incsuccess !='true' and id>0; 
+
+alter table v_db_host add urlcount MEDIUMINT(8) UNSIGNED NOT NULL COMMENT 'url收录数';
+alter table v_db_host add inccount MEDIUMINT(8) UNSIGNED NOT NULL COMMENT '成功收录数';
+alter table v_db_host add neartime int(10) UNSIGNED NOT NULL COMMENT '最近收录时间';
+
+/*更新urlcount*/
+update v_db_host h,(select hostid,count(hostid) as urlcount from v_db_url group by hostid) u 
+set h.urlcount=u.urlcount where h.id=u.hostid;
+/*更新inccount*/
+update v_db_host h,(select hostid,count(hostid) as inccount from v_db_url where incsuccess='true' group by hostid) u 
+set h.inccount=u.inccount where h.id=u.hostid;
+
+select * from v_db_host;
+
+/*关闭安全更新模式*/
+use iworm_db;
+set SQL_SAFE_UPDATES=0;
+
+select * from v_db_url where incsuccess='true';
+/*创建视图vi_url*/
+create view vi_url 
+as
+select u.id as id,hostid,url,urlcount,h.inccount as inccount,createtime 
+from v_db_url as u
+ 
+left join
+v_db_host as h
+
+on u.hostid=h.id
+
+where 1=1
+and u.incstate='false'
+and u.delstate='false'
+order by inccount desc,urlcount desc;
+
+select * from vi_url limit 0,1;
+select count(*) from v_db_url;
+select count(*) from v_db_url where incsuccess='true';
+
---
+++ data/sql/v_db_iworm.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 2016-03-29 11:58:23
+-- 服务器版本： 5.5.28
+-- PHP Version: 5.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `vyahui_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `v_db_host`
+--
+CREATE TABLE IF NOT EXISTS `v_db_host` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '关键字表id',
+  `hostname` VARCHAR(50) NOT NULL COMMENT '主机名',
+  `hostip` VARCHAR(30) NOT NULL COMMENT '主机对应ip',
+  `urlcount` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT 'url收录数',
+  `inccount` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT '获取文章数',
+  `inittime` INT(10) UNSIGNED NOT NULL COMMENT '第一次收录时间',
+  `neartime` INT(10) UNSIGNED NOT NULL COMMENT '最近收录时间',
+  `errcount` MEDIUMINT(4) UNSIGNED NOT NULL COMMENT '出错次数',
+  `isusing` ENUM('true', 'false') NOT NULL COMMENT '主机状态，是否还在运行',
+  PRIMARY KEY (`id`)
+  ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
+-- 表的结构 `v_db_url`
+--
+CREATE TABLE IF NOT EXISTS `v_db_url` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'urlid',
+  `hostid` INT(10) UNSIGNED NOT NULL,
+  `parenturl` VARCHAR(255) NOT NULL COMMENT '来源地址',
+  `url` VARCHAR(255) NOT NULL COMMENT '链接地址',
+  `incstate` ENUM('false', 'true') NOT NULL COMMENT '收录状态',
+  `createtime` INT(10) UNSIGNED NOT NULL COMMENT '收录时间',
+  `inctimes` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT '收录次数',
+  `incstate` ENUM('false', 'true') NOT NULL COMMENT '收录状态',
+  `incsuccess` ENUM('false', 'true') NOT NULL COMMENT '是否收录成功',
+  `delstate` ENUM('false', 'true') NOT NULL COMMENT '删除状态',
+  `deltime` INT(10) NOT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`)
+  ) ENGINE=MyISAM AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8;
+
+--
+-- 表的结构 `v_db_infolist`
+--
+CREATE TABLE IF NOT EXISTS `v_db_infolist` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'urlid',
+  `classid` SMALLINT(5) UNSIGNED NOT NULL COMMENT '文章分类id',
+  `title` VARCHAR(80) NOT NULL COMMENT '文章标题',
+  `flag` VARCHAR(30) NOT NULL COMMENT '属性',
+  `isoriginal` ENUM('false', 'true') NOT NULL COMMENT '是否原创',
+  `author` VARCHAR(50) NOT NULL COMMENT '收录时间',
+  `urlid` INT(10) UNSIGNED NOT NULL,
+  `keywords` VARCHAR(50) NOT NULL COMMENT '收录状态',
+  `description` VARCHAR(255) NOT NULL COMMENT '摘要',
+  `content` MEDIUMTEXT NOT NULL COMMENT '详细内容',
+  `picurl` VARCHAR(100) NOT NULL COMMENT '缩略图',
+  `picarr` TEXT NOT NULL COMMENT '图组',
+  `hits` MEDIUMINT(8) NOT NULL COMMENT '浏览数',
+  `orderid` INT(10) UNSIGNED NOT NULL COMMENT '排序',
+  `createtime` INT(10) UNSIGNED NOT NULL COMMENT '创建时间',
+  `checkinfo` ENUM('true', 'false') NOT NULL COMMENT '审核情况',
+  `delstate` ENUM('false', 'true') NOT NULL COMMENT '删除状态',
+  `deltime` INT(10) NOT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`)
+  ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
+-- 表的结构 `v_db_charset`
+--
+CREATE TABLE IF NOT EXISTS `v_db_charset` (
+  `id` SMALLINT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '字符编码id',
+  `hostid` INT(10) UNSIGNED NOT NULL COMMENT 'hostid',
+  `charset` VARCHAR(10) NOT NULL COMMENT '字符编码',
+  PRIMARY KEY (`id`)
+  ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
+-- 表的结构 `v_db_keywords`
+--
+CREATE TABLE IF NOT EXISTS `v_db_keywords` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '关键字表id',
+  `keyword` VARCHAR(50) NOT NULL COMMENT '关键字',
+  `searchtimes` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT '当天搜索次数',
+  `inittime` INT(10) UNSIGNED NOT NULL COMMENT '当天第一次搜索的时间',
+  PRIMARY KEY (`id`)
+  ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
